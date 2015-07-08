78d1abf5aebb871dc6ac6a3786d2cabb56759448
admin <admin@admin-PC>
2015-07-08 16:18:12 +0530

    First Commit to new Project GIT Respository

---
+++ Core Java/src/Deadlock.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+
+public class Deadlock extends Thread {
+public static void main(String args[])
+{
+Thread t1=new Thread();
+t1.
+
+}
+
+}
+
---
+++ Core Java/src/Thread.java
@@ -0,0 +1,12 @@
+
+public class Thread {
+public static void main(String args[])
+{
+	Deadlock d=new Deadlock();
+	
+	Thread t1=new Thread();
+	Thread t2=new Thread();
+	t1.start();
+	t2.
+}
+}
