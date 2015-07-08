4c49365565099a7437be708a40affa120732423e
admin <admin@admin-PC>
2015-07-08 16:24:55 +0530

    Modified Code in Threads

--- Core Java/src/Thread.java
+++ Core Java/src/Thread.java
@@ -1,12 +1,22 @@
 
-public class Thread {
+public class Thread implements Runnable{
 public static void main(String args[])
 {
 	Deadlock d=new Deadlock();
 	
 	Thread t1=new Thread();
+	
 	Thread t2=new Thread();
-	t1.start();
-	t2.
+	System.out.println("First Thread");
+	t1.run();
+	System.out.println("Second Thread");
+	t2.run();
+
+}
+
+@Override
+public void run() {
+	// TODO Auto-generated method stub
+	System.out.println("welcome to threads");
 }
 }
